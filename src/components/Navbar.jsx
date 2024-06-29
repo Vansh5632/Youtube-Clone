f1bda9745677799d2624e6ffb86ef4433b07700a
Vansh <vanshgilhotra8885@gmail.com>
2024-06-29 17:31:14 +0530

    NavbarCompleted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import image from "../assets/youtube-svgrepo-com.svg";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdOutlineVideoCall } from "react-icons/md";
+import { CiBellOn } from "react-icons/ci";
 
 const Navbar = () => {
-  return (
-    <div className="flex justify-between items-center bg-[#212121] opacity-95 h-13 py-8 px-3 sticky">
-      <div className="flex gap-8 items-center text-2xl text-white">
-        <div className="h-13 w-15">
-          <RxHamburgerMenu/>
+    return (
+        <div className="flex justify-between items-center bg-[#212121] opacity-95 h-16 px-3 sticky">
+            <div className="flex gap-8 items-center text-2xl text-white">
+                <div className="h-13 w-15">
+                    <RxHamburgerMenu className="text-white" />
+                </div>
+                <div className="h-10 w-14 flex">
+                    <img src={image} alt=""/>
+                    <span className="text-3xl mx-4 font-bold">YOUTUBE</span>
+                </div>
+            </div>
+            <div className="flex items-center justify-center flex-grow">
+                <form>
+                    <div className="flex items-center justify-center">
+                        <div className="flex gap-5 items-center pr-5 h-5">
+                            <input type="text" placeholder="Search" className="bg-transparent border-stone-600 border-2 h-10 text-[30px] rounded-[15px] p-4" />
+                        </div>
+                        <button>
+                            <AiOutlineSearch className="text-[30px] text-white flex items-center" />
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="ml-3">
+                <MdOutlineVideoCall className="text-white text-[30px]"  />
+            </div>
+            <div className="ml-3">
+                <CiBellOn className="text-white text-[30px]"/>
+            </div>
         </div>
-        <div className="h-10 w-14 flex">
-          <img src={image} alt=""/>
-          <span className="text-3xl mx-4 font-bold">YOUTUBE</span>
-        </div>
-        <div>
-            
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
